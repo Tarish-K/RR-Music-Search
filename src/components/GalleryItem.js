7663e82818b2fc37b9f4d0ba0ee3bec785535e4a
Tarish K <102713730+Tarish-K@users.noreply.github.com>
2022-10-02 19:01:27 -0400

    finished

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,53 +1,19 @@
-// GalleryItem.js
-import { useState } from 'react' 
+import GalleryItem from './GalleryItem'
 
-function GalleryItem(props){
-    let [view, setView] = useState(false)
+function Gallery(props){
 
-    const simpleStyle = {
-        'width': '25vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px'
-    }
-    
-    const detailStyle = {
-        'width': '80vw',
-        'height': '20vh',
-        'border': '1px solid black',
-        'margin': '2px',
-        'backgroundImage': `url(${props.item.artworkUrl100})`,
-        'backgroundRepeat': 'no-repeat',
-        'backgroundSize': 'cover',
-        'color': 'yellow'
-    }
-
-    const simpleView = () => {
+    const display = props.data.map((item, index) => {
         return (
-            <div style={simpleStyle}>
-                <h3>{props.item.trackName}</h3>
-                <h4>{props.item.collectionName}</h4>
-            </div>
+            <GalleryItem item={item} key={index} />
         )
-    }
-
-    const detailView = () => {
-        return (
-            <div style={detailStyle}>
-                <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
-                <h4>{props.item.primaryGenreName}</h4>
-                <h4>{props.item.releaseDate}</h4>
-            </div>
-        )
-    }
+    })
 
     return (
-        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
-            <p>One Gallery Item</p>
-                {view ? detailView() : simpleView()}
+        <div style={{textAlign: 'center'}}>
+        {display}
         </div>
     )
 }
 
-export default GalleryItem
+
+export default Galler
